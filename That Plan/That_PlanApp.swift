3c0d1a770f221ad4840988fbb9ef9a1a11345f78
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2025-02-17 19:53:12 +0900

    Initial commit

---
+++ That Plan/That_PlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  That_PlanApp.swift
+//  That Plan
+//
+//  Created by Jiyoung Park on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct That_PlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
