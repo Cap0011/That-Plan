a18d2e44756b69f26294d4c1c6cde050c1af6380
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2025-03-24 21:51:21 +0900

    Feat: Add DailyRoutineView

---
+++ That Plan/View/DailyRoutineView.swift
@@ -0,0 +1,63 @@
+//
+//  DailyRoutineView.swift
+//  That Plan
+//
+//  Created by Jiyoung Park on 3/24/25.
+//
+
+import SwiftUI
+
+struct DailyRoutineView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        entity: CDTask.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \CDTask.createdAt, ascending: true)],
+        predicate: NSPredicate(format: "type == %@", TaskType.daily.text)
+    ) var routines: FetchedResults<CDTask>
+    
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+            
+            ScrollView {
+                VStack(spacing: 9) {
+                    ForEach(Array(routines), id: \.id) { routine in
+                        NavigationLink(destination: NoteDetailView(note: routine)) {
+                            if let contents = routine.contents {
+                                TaskItemView(type: "Daily", content: contents)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 15)
+                .padding(.horizontal, 19)
+                .background(.white)
+            }
+            .scrollIndicators(.hidden)
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image(systemName: "chevron.backward")
+                        .foregroundStyle(.backgray)
+                        .frame(width: 40, height: 40)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    Text("Daily Routine")
+                        .font(.EBGaramond18)
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DailyRoutineView()
+}
