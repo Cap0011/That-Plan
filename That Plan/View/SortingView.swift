5760e284139de49cb6eecc103dab994aee126899
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2025-04-29 20:46:00 +0900

    Design: Update SortingView design

--- That Plan/View/SortingView.swift
+++ That Plan/View/SortingView.swift
@@ -12,6 +12,7 @@ struct SortingView: View {
     let text: String
     
     @State private var selectedIndex: Int?
+    @State private var isPopupPresented = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
@@ -45,30 +46,46 @@ struct SortingView: View {
             .padding(.vertical, 15)
             
             HStack(spacing: 10) {
-                TypeView(title: "Detailed\nTask", index: 0, padding: 27, height: 150, selectedIndex: $selectedIndex)
+                TypeView(title: "Detailed\nTask", index: 0, padding: 0, height: 150, selectedIndex: $selectedIndex)
                 
-                TypeView(title: "Daily\nRoutine", index: 1, padding: 27, height: 150, selectedIndex: $selectedIndex)
+                TypeView(title: "Daily\nRoutine", index: 1, padding: 0, height: 150, selectedIndex: $selectedIndex)
                 
-                TypeView(title: "Information", index: 2, padding: 27, height: 150, selectedIndex: $selectedIndex)
+                TypeView(title: "Information", index: 2, padding: 10, height: 150, selectedIndex: $selectedIndex)
             }
             
             HStack(spacing: 12) {
-                TypeView(title: "Short-term Goal", index: 3, padding: 19, height: 109, selectedIndex: $selectedIndex)
+                TypeView(title: "Short-term Goal", index: 3, padding: 0, height: 116, selectedIndex: $selectedIndex)
                 
-                TypeView(title: "Future Goal", index: 4, padding: 19, height: 109, selectedIndex: $selectedIndex)
+                TypeView(title: "Future Goal", index: 4, padding: 0, height: 116, selectedIndex: $selectedIndex)
             }
             .padding(.top, 10)
             
             if let index = selectedIndex {
-                NavigationLink(destination: destinationView(for: index)) {
-                    nextButton
-                        .padding(.top, 35)
+                if index == 0 || index == 3 {
+                    NavigationLink(destination: destinationView(for: index)) {
+                        nextButton
+                            .padding(.top, 35)
+                    }
+                } else {
+                    if index == 1 {
+                        // TODO: Return to HomeView and show toast message
+                        nextButton
+                            .onTapGesture {
+                                Utility.resetToRootView()
+                            }
+                    } else {
+                        // TODO: Return to HomeView and show popup
+                        nextButton
+                            .onTapGesture {
+                                Utility.resetToRootView()
+                            }
+                    }
                 }
             } else {
                 nextButton
                     .padding(.top, 35)
                     .onTapGesture {
-                        // TODO: Alert !
+                        isPopupPresented.toggle()
                     }
             }
             
@@ -76,6 +93,10 @@ struct SortingView: View {
         }
         .padding(.top, 15)
         .padding(.horizontal, 20)
+        .popup(isPresented: $isPopupPresented) {
+            selectPopup
+                .offset(y: -50)
+        }
         .background(.white)
         .navigationBarBackButtonHidden()
         .navigationBarTitleDisplayMode(.inline)
@@ -95,14 +116,14 @@ struct SortingView: View {
                     NavigationLink(destination: destinationView(for: index)) {
                         Text("Next")
                             .font(.EBGaramond19)
-                            .foregroundStyle(.nextgreen)
+                            .foregroundStyle(Utility.mainColor.opacity(0.7))
                     }
                 } else {
                     Text("Next")
                         .font(.EBGaramond19)
-                        .foregroundStyle(.nextgreen)
+                        .foregroundStyle(Utility.mainColor.opacity(0.7))
                         .onTapGesture {
-                            // TODO: Alert !
+                            isPopupPresented.toggle()
                         }
                 }
             }
@@ -113,11 +134,47 @@ struct SortingView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .frame(height: 50)
-                .foregroundStyle(.monthgreen)
+                .foregroundStyle(Utility.mainColor)
             
             Text("Next")
                 .font(.EBGaramond19)
                 .foregroundStyle(.white)
+        }
+    }
+    
+    var selectPopup: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.white)
+                .frame(width: 297, height: 193)
+            
+            VStack(spacing: 0) {
+                Text("Reminder")
+                    .font(.custom("Pretendard-SemiBold", size: 19))
+                    .foregroundStyle(.gray800)
+                    
+                Text("Select a task type to continue.")
+                    .font(.custom("Pretendard-Regular", size: 16))
+                    .foregroundStyle(.gray700)
+                    .padding(.top, 12)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(Utility.mainColor)
+                        .frame(width: 265, height: 43)
+                    
+                    Text("Got it")
+                        .font(.custom("Pretendard-Medium", size: 15))
+                        .foregroundStyle(.white)
+                }
+                .padding(.top, 36)
+                .onTapGesture {
+                    isPopupPresented.toggle()
+                }
+            }
+            .padding(.top, 45)
+            .padding(.horizontal, 16)
+            .padding(.bottom, 17)
         }
     }
     
@@ -130,16 +187,23 @@ struct SortingView: View {
         @Binding var selectedIndex: Int?
         
         var body: some View {
-            ZStack(alignment: .bottom) {
+            ZStack(alignment: .center) {
                 RoundedRectangle(cornerRadius: 15)
+                    .strokeBorder(selectedIndex == index ? Utility.mainColor : .clear, lineWidth: 4)
+                    .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.black))
                     .foregroundStyle(.black)
                     .frame(height: height)
                 
-                Text(title)
-                    .font(.EBGaramondMedium16)
-                    .foregroundStyle(.white)
-                    .padding(.bottom, padding)
-                    .multilineTextAlignment(.center)
+                VStack(spacing: 14 + padding) {
+                    Image("type\(index)")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                    
+                    Text(title)
+                        .font(.EBGaramondMedium16)
+                        .foregroundStyle(selectedIndex == index ? Utility.mainColor : .white)
+                        .multilineTextAlignment(.center)
+                }
             }
             .onTapGesture {
                 selectedIndex = index
@@ -148,14 +212,13 @@ struct SortingView: View {
     }
     
     func destinationView(for index: Int) -> AnyView {
-        // TODO: Add different views on index
         switch index {
         case 0:
             return AnyView(DetailedTaskView(text: text))
         case 3:
             return AnyView(ShortGoalView(text: text))
         default:
-            return AnyView(WritingView())
+            return AnyView(HomeView())
         }
     }
 }
